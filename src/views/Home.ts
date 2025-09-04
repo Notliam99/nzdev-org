d0dda4a66872e207c7864c71c1630d670058ea39
Liam Tietjens <l.tietjens@hotmail.com>
2025-09-04 21:37:21 +1200

    wow

--- src/views/Home.ts
+++ src/views/Home.ts
@@ -12,7 +12,7 @@ export default class extends AbstractView {
       <h2>This is the index of the site.</h2>
       <p>
         the center of the hole website lies here.
-        <b>very cool!!!</b> Check The About Bellow.
+        <b>very cool!!!</b> Check The About Down Bellowwwww.
       </p>
       <a href="/about" data-link>About (<b>Nz Dev</b>)</a>
     `;
