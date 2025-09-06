11795a133a214b6576fe70016423cf77fd8b9630
Liam Tietjens <l.tietjens@hotmail.com>
2025-09-07 00:43:16 +1200

    added a second section to the sight with futher explaination and a contact info section

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,42 @@ function App() {
 
     return (
         <>
-            <Hero />
-            <p class="m-[1rem] text-center font-medium">
-                We are a small tech company based in queenstown.
-                Just having a vibe making some techy stuff
-            </p>
-            <div class="h-[2rem]">
+            <Hero height="h-dvh" footnote="Wellcome" />
+            <div class="flex justify-center items-center mx-auto">
+                <p class="m-[1.5rem] max-w-[800px] text-center font-medium leading-[1.5rem]">
+                    We are a small tech company based in queenstown.
+                    Just having a vibe, making some techy stuff.
+                    Like this snazzy website cooked up in two days.
+                </p>
             </div>
+            <Hero height="h-[20dvw] min-[10rem]:h-[60dvh] @min-[10rem]:h-[60dvh]" footnote="Extremely Real 👻" />
+            <h1 class="mt-[1rem] mr-[1rem] ml-[1rem] text-center font-bold leading-[1.5rem]">
+                We Are Real
+            </h1>
+            <div class="flex justify-center items-center mx-auto">
+                <p class="m-[1.5rem] text-center text-pretty font-medium leading-[1.5rem] max-w-[800px]">
+                    Like you call up the govement they throeticaly would be like yo <i>"NZ DEV LIMMITED is a real commpany..."</i> theroreticaly.
+                    We have some of the highest {"<blank>"} in the world.
+                    One member that can count to nearly five or six with both hands.
+                    The talent at NZ DEV LIMMITED is unlike anywhere else.
+                </p>
+            </div>
+            <hr class="h-0 border-0 shadow-[0_0_10px_1px_black] relative after:content-['\00a0'] after:block" />
+            <p class="m-[3rem] text-center font-medium leading-[1.5rem]">
+                Contact Us At: <a
+                    class="
+                        text-[transparent]
+                        transition-[text-shadow, font-weight]
+                        delay-[150ms,_50ms] duration-500
+                        ease-[cubic-bezier(0.88,_-1,_0.2,_1)]
+                        text-shadow-[0_0_5px_#000]
+                        hover:text-shadow-[0_0_1px_#000]
+                        hover:font-medium max-[10rem]:font-light
+                        min-[10rem]:text-shadow-[0_0_1.8px_#000]"
+                    href="mailto:specil.people.contact@nzdev.org">
+                    specil.people.contact@nzdev.org
+                </a>
+            </p>
         </>
     )
 }
--- src/hero.tsx
+++ src/hero.tsx
@@ -1,18 +1,40 @@
-function hero() {
+interface hero_props {
+    footnote: string
+    // must be a tailwind class
+    height: string
+}
+
+function hero(props: hero_props) {
     return (
         <>
-            <div class={"h-screen relitive flex align-middle items-center flex-col @container"}>
+            <div class={props.height.replace("@", "") + " relitive flex align-middle items-center flex-col @container"}>
                 <div class="flex items-center justify-center flex-col grow">
-                    <p class="text-[19rem] font-[250] @sm:text-[6rem]">
+                    <p class="text-[19rem] font-[250] @min-[10rem]:text-[6rem]">
                         NZ DEV
                     </p>
-                <div class="flex items-center justify-center">
-                    <p class="text-[10rem] @sm:text-[3.5rem] font-[300]">
-                        Wellcome
-                    </p>
+                    <div class="flex items-center justify-center">
+                        <p class="text-[10rem] @min-[10rem]:text-[3.5rem] font-[300] text-center m-[1rem]">
+                            {props.footnote}
+                        </p>
+                    </div>
                 </div>
-                </div>
-                <div class={"g-white/30 backdrop-blur-sm @sm:backdrop-blur-xs absolute top-0 left-0 h-screen w-screen"}>
+                <div
+                    class={props.height + " g-white/30 backdrop-blur-xs absolute inset w-[100dvw]"}
+                    style="
+                        -webkit-mask-image: linear-gradient(to bottom,
+                            transparent 0%,
+                            white 40px,
+                            white calc(100% - 40px),
+                            transparent 100%
+                        );
+                        mask-image: linear-gradient(to bottom,
+                            transparent 0%,
+                            white 40px,
+                            white calc(100% - 40px),
+                            transparent 100%
+                        );
+                    "
+                >
                 </div>
             </div >
         </>
