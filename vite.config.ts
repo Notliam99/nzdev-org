5ca43cbf80f63deb4fe273072f7f71a5b0616c7c
Liam Tietjens <l.tietjens@hotmail.com>
2025-09-06 19:43:32 +1200

    update allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import solid from 'vite-plugin-solid'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tailwindcss(), solid()],
+    plugins: [tailwindcss(), solid()],
+    preview: {
+        allowedHosts: ["nzdev.org", "*.nzdev.org", "127.0.0.1", "localhost"]
+    }
 })
